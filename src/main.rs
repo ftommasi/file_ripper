42094d25f0814cabe4f21ddc4a3d0b758bca5277
Fausto Tommasi <fgtommasi@gmail.com>
2024-02-19 10:36:58 -0600

    update Search Result data structure and implement Levneshtein distance for fuzzy file search

--- src/main.rs
+++ src/main.rs
@@ -29,11 +29,27 @@ fn main()-> Result<(), eframe::Error> {
     )
 }
 
+struct SearchResult{
+    result_string : String,
+    result_full_path : String,
+    result_search_score : u32,
+}
+
+impl Default for SearchResult{
+    fn default() -> Self {
+        SearchResult{
+            result_string : String::from(""),
+            result_full_path : String::from(""),
+            result_search_score : 0,
+        }
+    }
+}
+
 struct FileRipper {
     name : String,
     cur_path : String, //want to use but borrowing is sucking Box<path::Path>,
     search_term : String, 
-    search_results : Vec<String>,
+    search_results : Vec<SearchResult>,
 }
 
 
@@ -92,28 +108,33 @@ impl eframe::App for FileRipper{
                 ui.vertical(|ui| {
                    ui.add_space(30.0);
                    ui.separator();
-                   ui.heading("serach...");
-                   if ui.text_edit_singleline(&mut self.search_term).lost_focus() && ctx.input(|i| i.key_pressed(Key::Enter)){
+                   ui.heading("search...");
+                   ui.text_edit_singleline(&mut self.search_term);
+                   if 
+                       //ui.text_edit_singleline(&mut self.search_term).lost_focus() && 
+                       is_any_key_down(ctx)
+
+                       {
                         //do ripgrep open dialog test
 
                         self.search_results = crawl_subdirs(self.cur_path.clone()); //@SPEED
-                        
-                        let mut min_lev_score = 99999; //What should be the initial value?
-                        for entry in &self.search_results{
-                            let entry_clone = entry.clone(); //@SPEED
-                            let entry_score = levenshtein_distance(self.search_term.clone(), entry.to_owned()); //@SPEED
-                            if min_lev_score < entry_score{
-                                min_lev_score = entry_score;
-                                println!("word: {} score: {}\n",self.search_term.clone(),entry_score);
-                            }
+                        for entry in &mut self.search_results{
+                            
+                            //let entry_clone = entry.clone(); //@SPEED
+                            //let entry_score = 0;
+                            /* The code to compute the levenshtein_distance makes the input run
+                             * very slowly especially the longer the search term gets*/
+                            let entry_score = levenshtein_distance(self.search_term.clone(), entry.result_string.to_owned()); //@SPEED
+                            entry.result_search_score = entry_score;
                         }
                    }
 
                    for entry in &self.search_results{
-                       let entry_clone = entry.clone(); //@SPEED
-                       ui.horizontal(|ui|{
-                           ui.label(entry_clone);
-                       });
+                       if entry.result_search_score == entry.result_string.len().try_into().unwrap(){
+                           ui.horizontal(|ui|{
+                               ui.label(&entry.result_full_path);
+                            });
+                        }
                    }
                 });
 
@@ -123,8 +144,9 @@ impl eframe::App for FileRipper{
 
 }
 
-pub fn crawl_subdirs(cur_dir : String) -> Vec<String>{
-    let mut sub_dirs : Vec<String> = vec![];
+
+pub fn crawl_subdirs(cur_dir : String) -> Vec<SearchResult>{
+    let mut sub_dirs : Vec<SearchResult> = vec![];
     //lets go depth first down until we run out of directories to build our set
     //to compute distance to
     for dir in std::fs::read_dir(cur_dir).unwrap(){
@@ -132,7 +154,14 @@ pub fn crawl_subdirs(cur_dir : String) -> Vec<String>{
             sub_dirs.append(&mut crawl_subdirs(String::from(dir.unwrap().path().to_str().unwrap())));
         }
         else{
-            sub_dirs.push(String::from(dir.unwrap().path().to_str().unwrap()));
+            //sub_dirs.push(String::from(dir.unwrap().path().to_str().unwrap()));
+            let full_dir = String::from(dir.as_ref().unwrap().path().to_str().unwrap());
+            let filename = String::from(dir.as_ref().unwrap().file_name().to_str().unwrap());
+            sub_dirs.push(SearchResult{
+                result_string : filename.clone(),
+                result_full_path : full_dir,
+                result_search_score : filename.len().try_into().unwrap(),
+            });
         }
     }
     sub_dirs
@@ -175,4 +204,87 @@ pub fn levenshtein_distance(s1 : String, s2 : String) -> u32{
         }
     }
     current[s2_len-1].try_into().unwrap()
+}
+
+pub fn is_any_key_down(ctx : &egui::Context) -> bool {
+    //TODO(ftommasi) is there a better way to do this ??
+    ctx.input(|i| i. keys_down.len() > 0)
+    /*
+    if ctx.input(|i| 
+                 i.key_pressed(Key::Q)
+              || i.key_pressed(Key::W)
+              || i.key_pressed(Key::E)
+              || i.key_pressed(Key::R)
+              || i.key_pressed(Key::T)
+              || i.key_pressed(Key::Y)
+              || i.key_pressed(Key::U)
+              || i.key_pressed(Key::I)
+              || i.key_pressed(Key::O)
+              || i.key_pressed(Key::P)
+
+              || i.key_pressed(Key::A)
+              || i.key_pressed(Key::S)
+              || i.key_pressed(Key::D)
+              || i.key_pressed(Key::F)
+              || i.key_pressed(Key::G)
+              || i.key_pressed(Key::H)
+              || i.key_pressed(Key::J)
+              || i.key_pressed(Key::K)
+              || i.key_pressed(Key::L)
+
+              || i.key_pressed(Key::Z)
+              || i.key_pressed(Key::X)
+              || i.key_pressed(Key::C)
+              || i.key_pressed(Key::V)
+              || i.key_pressed(Key::B)
+              || i.key_pressed(Key::N)
+              || i.key_pressed(Key::M)
+
+              || i.key_pressed(Key::ArrowDown)
+              || i.key_pressed(Key::ArrowLeft)
+              || i.key_pressed(Key::ArrowRight)
+              || i.key_pressed(Key::ArrowUp)
+              || i.key_pressed(Key::Escape)
+              || i.key_pressed(Key::Tab)
+              || i.key_pressed(Key::Backspace)
+              || i.key_pressed(Key::Enter)
+              || i.key_pressed(Key::Space)
+              || i.key_pressed(Key::Insert)
+              || i.key_pressed(Key::Delete)
+              || i.key_pressed(Key::Home)
+              || i.key_pressed(Key::End)
+              || i.key_pressed(Key::PageUp)
+              || i.key_pressed(Key::PageDown)
+              || i.key_pressed(Key::Copy)
+              || i.key_pressed(Key::Cut)
+              || i.key_pressed(Key::Paste)
+              || i.key_pressed(Key::Colon)
+              || i.key_pressed(Key::Comma)
+              || i.key_pressed(Key::Backslash)
+              || i.key_pressed(Key::Slash)
+              || i.key_pressed(Key::Pipe)
+              || i.key_pressed(Key::Questionmark)
+              || i.key_pressed(Key::OpenBracket)
+              || i.key_pressed(Key::CloseBracket)
+              || i.key_pressed(Key::Backtick)
+              || i.key_pressed(Key::Minus)
+              || i.key_pressed(Key::Period)
+              || i.key_pressed(Key::Plus)
+              || i.key_pressed(Key::Equals)
+              || i.key_pressed(Key::Semicolon)
+              || i.key_pressed(Key::Num0)
+              || i.key_pressed(Key::Num1)
+              || i.key_pressed(Key::Num2)
+              || i.key_pressed(Key::Num3)
+              || i.key_pressed(Key::Num4)
+              || i.key_pressed(Key::Num5)
+              || i.key_pressed(Key::Num6)
+              || i.key_pressed(Key::Num7)
+              || i.key_pressed(Key::Num8)
+              || i.key_pressed(Key::Num9)
+              ){
+        return true;
+    }
+    false
+        */
 }
